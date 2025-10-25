3b40556808240f5fb3836a38d3980c3cdda8e7b3
qianshuuhan <1652532319@qq.com>
2025-10-25 21:02:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
+import math
+
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, num):
+    for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             return False
     return True
-
-def PrimeList(N):
-    primes = []
-    for num in range(2, N):
-        if is_prime(num):
-            primes.append(str(num))
-    return ' '.join(primes)
-
-# 测试示例
-print(PrimeList(10))  # 输出：2 3 5 7
-print(PrimeList(20))  # 输出：2 3 5 7 11 13 17 19
